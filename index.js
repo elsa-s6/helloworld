ddb13c25797c69fb776b2eda8a43641b892f5a71
Elsa Gómez <elsa.gomez@lingokids.com>
2022-05-13 12:05:57 +0200

    Eigth task

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('Hello world');
+// Part one
+
+for (let i = 0; i < 11; i++) {
+    i + 1;
+    console.log(i);
+  }
+
+// Part two 
+
+var number = 5;
+var x = 0;
+while(x < number){
+  x = Math.floor((Math.random() * 9) + 1);
+  console.log(x);
+  }
