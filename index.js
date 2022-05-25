7909064167bae47f8f1aa1ea37314f973be15675
Elsa Gómez <elsa.gomez@lingokids.com>
2022-05-25 14:59:06 +0200

    9th to 11th tasks

--- index.js
+++ index.js
@@ -1,15 +1,79 @@
-// Part one
+// Task 9 
 
-for (let i = 0; i < 11; i++) {
-    i + 1;
-    console.log(i);
+try{
+
+  let randomNum = Math.floor((Math.random() * 9) + 1)
+
+  switch (randomNum) {
+    case 6:
+      throw "Six";
+      break;
+    case 7:
+      throw "Seven";
+      break;
+    case 8:
+      throw "Eight";
+      break;
+    case 8:
+      throw "Nine";
+      break;
+    default:
+      console.log(`${randomNum}`)
   }
+}catch (error){
+	console.log(`${error}`); //print in the console the message from the error.
+}finally{
+	console.log("Good bye");
+}
 
-// Part two 
+// Task 10 
 
-var number = 5;
-var x = 0;
-while(x < number){
-  x = Math.floor((Math.random() * 9) + 1);
-  console.log(x);
+class Game {
+	constructor(name, platform, price){
+		this.name = name;
+		this.platform = platform;
+		this.price = price;
+	}
+
+  addEuro(){
+    return this.price++;
+  };
+}
+
+let animalCrossing = new Game ("Animal Crossing", "Nintendo Switch", 60);
+console.log(animalCrossing.platform);
+
+let genshinImpact = new Game ("Genshin Impact", "Play Station 4, PC/Macintosh and iOS/Android", 0);
+console.log(genshinImpact.platform);
+
+
+animalCrossing.addEuro();
+console.log(animalCrossing.price);
+
+genshinImpact.addEuro();
+console.log(genshinImpact.price);
+
+
+
+// Task 11
+
+function myDisplayer(some) {
+  console.log(some);
+}
+
+let myPromise = new Promise(function(myResolve, myReject) {
+  let x = 1;
+
+// The producing code (this may take some time)
+
+  if (x == 0) {
+    myResolve("OK");
+  } else {
+    myReject("Error");
   }
+});
+
+myPromise.then(
+  function(value) {myDisplayer(value);},
+  function(error) {myDisplayer(error);}
+);
