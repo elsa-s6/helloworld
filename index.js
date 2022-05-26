52296cf0346128c68a06d32e662cd8ec175dedc2
Elsa Gómez <elsa.gomez@lingokids.com>
2022-05-26 16:58:42 +0200

    Last task

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('Hello world');
+function myDisplayer(some) {
+  console.log(some);
+};
+
+async function myFunction(){
+
+  let myPromise = new Promise(function(resolve, reject){
+    let x = 1;
+
+    if (x == 0) {
+      resolve("OK");
+    } else {
+      reject("Error");
+    }; 
+
+  })
+
+myDisplayer(await myPromise);
+
+};
+
+myFunction();
